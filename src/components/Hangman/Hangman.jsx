d246e363a1a1cc1492d08f95db7ecb8688ffc636
Xenon <gonda.armand@gmail.com>
2021-05-17 16:28:05 +0200

    Generated virtual keyboard.

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -14,20 +14,31 @@ export default class Hangman extends React.Component {
             guess: new Set([])
         }
     }
+    // Reveal the character if it was guessed correctly.
     guessedLetter = () => {
-        this.state.answer.split("")
-        .map(letter => 
-            this.state.guess.has(letter) ? letter : " _ "
-        );
+        return this.state.answer.split("")
+            .map(letter => 
+                this.state.guess.has(letter) ? letter : " _ "
+            );
+    }
+    // Create a button for each letter of the English alphabet. Disable buttons which have had been clicked.
+    generateKeyboard = () => {
+        let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
+        console.log(alphabet);
+        return alphabet.map(letter => 
+            <button key={letter} value={letter}>{letter}</button>
+        )
     }
     render() {
         const gameOver = this.state.mistakes >= this.props.maxMistakes;
+        let keyboard = this.generateKeyboard();
         return(
             <div id="wrapper">
                 <article id="hangman-container">
                     <h1 id="hangman-title">The Hangman</h1>
                     <p>Guess the word!</p>
                     <p>{gameOver ? `Correct word: ${this.state.answer}` : this.guessedLetter()}</p>
+                    <p>{keyboard}</p>
                     {/* <p>{this.state.answer}</p> */}
                 </article>
             </div>
