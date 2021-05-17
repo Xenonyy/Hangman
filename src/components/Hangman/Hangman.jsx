3dba7ac69b22d65bdea58fdfd0fcb8b6363bb3db
Xenon <gonda.armand@gmail.com>
2021-05-17 16:02:37 +0200

    Added state. Getting new word on each refresh.

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -3,10 +3,20 @@ import './Hangman.css'
 import word from '../Words/Words';
 
 export default class Hangman extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mistakes: 0,
+            answer: word()
+        }
+    }
     render() {
         return(
             <div id="wrapper">
-                <h1>The Hangman</h1>
+                <article id="hangman-container">
+                    <h1 id="hangman-title">The Hangman</h1>
+                    <p>{this.state.answer}</p>
+                </article>
             </div>
         )
     }
