7944853fe5d281ac0c4bfbbfe2fa27acd3429d24
Xenon <gonda.armand@gmail.com>
2021-05-17 18:45:01 +0200

    Added the new game panel, now fully functioning. User is able to select word length, and upon clicking new game the selection screen will come up again.

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -45,11 +45,34 @@ export default class Hangman extends React.Component {
             guess: new Set([]),
             answer: word.getRandom()
         })
+        document.querySelector("#hangman-game").classList.toggle("hide");
+        document.querySelector("#hangman-newgame").classList.toggle("hide");
+    }
+    handleWordLength = (e) => {
+        let length = e.target.value;
+        console.log(length, this.state.answer.length)
+        let correctLength = [];
+        // console.log(word.getFullArr)
+        // Will throw an error with strict equal!
+        word.getFullArr.map(word => word.length == length ? correctLength.push(word) : word); // eslint-disable-line
+        console.log(correctLength)
+        // console.log(findLength, correctLength)
+        const findRandom = () => correctLength[Math.floor(Math.random() * correctLength.length)];
+        // console.log(findRandom)
+        this.setState({
+            answer: findRandom()
+        });
+        console.log(this.state.answer)
     }
     generateWordLength = () => {
         let length = [...Array(7).keys()];
         return length.map(i =>
-            <button key={i}>{word.getMinLenght + i}</button>)
+            <button key={word.getMinLenght + i} value={word.getMinLenght + i} onClick={this.handleWordLength}>{word.getMinLenght + i}</button>
+        )
+    }
+    startGame = () => {
+        document.querySelector("#hangman-game").classList.toggle("hide");
+        document.querySelector("#hangman-newgame").classList.toggle("hide");
     }
     render() {
         // Calculate the outcome of the game and display the state accordingly.
@@ -62,10 +85,9 @@ export default class Hangman extends React.Component {
         if (correctAnswer) result = "You've won";
         if (gameOver) result = "You've lost";
         
-
         return(
             <div id="wrapper">
-                <article className="hangman-container">
+                <article className="hangman-container" id="hangman-newgame">
                     <h1 className="hangman-title">The Hangman</h1>
                     <div id="new-game-container">
                         <p>Let's play <b>Hangman!</b></p>
@@ -73,9 +95,10 @@ export default class Hangman extends React.Component {
                     </div>
                     <div id="word-select-container">
                         <p>{this.generateWordLength()}</p>
+                        <button id="play-btn" value={this.state.answer.length} onClick={() => {this.startGame();}}>Let's play!</button>
                     </div>
                 </article>
-                <article className="hangman-container hide">
+                <article className="hangman-container hide" id="hangman-game">
                     <h1 className="hangman-title">The Hangman</h1>
                     <p>{result}</p>
                     {/* <p>{this.state.answer}</p> */}
@@ -83,8 +106,8 @@ export default class Hangman extends React.Component {
                     <p id="keyboard-text">Play with a word</p>
                     <p id = "keyboard">{this.generateKeyboard()}</p>
                     <div id="button-container">
-                        <button className="game-btn" onClick={this.endGame}>END GAME</button>
-                        <button className="game-btn" onClick={this.resetGame}>START NEW GAME</button>
+                        <button className="game-btn" onClick={this.endGame}>End Game</button>
+                        <button className="game-btn" onClick={this.resetGame}>Start New Game</button>
                     </div>
                 </article>
             </div>
