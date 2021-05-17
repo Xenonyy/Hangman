a8f1915a535c036f34f5a7bcce5548e3d7019945
Xenon <gonda.armand@gmail.com>
2021-05-17 16:23:04 +0200

    Added basic game mechanics like guessing and mistakes.

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -3,19 +3,32 @@ import './Hangman.css'
 import word from '../Words/Words';
 
 export default class Hangman extends React.Component {
+    static defaultProps = {
+        maxMistakes: 6
+    }
     constructor(props) {
         super(props);
         this.state = {
             mistakes: 0,
-            answer: word()
+            answer: word(),
+            guess: new Set([])
         }
     }
+    guessedLetter = () => {
+        this.state.answer.split("")
+        .map(letter => 
+            this.state.guess.has(letter) ? letter : " _ "
+        );
+    }
     render() {
+        const gameOver = this.state.mistakes >= this.props.maxMistakes;
         return(
             <div id="wrapper">
                 <article id="hangman-container">
                     <h1 id="hangman-title">The Hangman</h1>
-                    <p>{this.state.answer}</p>
+                    <p>Guess the word!</p>
+                    <p>{gameOver ? `Correct word: ${this.state.answer}` : this.guessedLetter()}</p>
+                    {/* <p>{this.state.answer}</p> */}
                 </article>
             </div>
         )
