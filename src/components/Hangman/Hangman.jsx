894ef3af8287ca79c923da8624136cb29d4a43c2
Xenon <gonda.armand@gmail.com>
2021-05-17 15:26:38 +0200

    Set up hangman component.

---
+++ src/components/Hangman/Hangman.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hangman.css'
+import word from '../Words/Words';
+
+export default class Hangman extends React.Component {
+    render() {
+        return(
+            <div id="wrapper">
+                <h1>The Hangman</h1>
+            </div>
+        )
+    }
+}
