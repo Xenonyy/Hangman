bd81b9adbb059ecdc1245c97600b142811130cad
Xenon <gonda.armand@gmail.com>
2021-05-17 19:59:37 +0200

    Added images.

---
+++ src/components/Images/Images.js
@@ -0,0 +1,11 @@
+import { ReactComponent as SVG } from './Images/0.svg';
+import { ReactComponent as SVG1 } from './Images/1.svg';
+import { ReactComponent as SVG2 } from './Images/2.svg';
+import { ReactComponent as SVG3 } from './Images/3.svg';
+import { ReactComponent as SVG4 } from './Images/4.svg';
+import { ReactComponent as SVG5 } from './Images/5.svg';
+import { ReactComponent as SVG6 } from './Images/6.svg';
+
+// Put all the imported images in an array and export them 
+const imageArr = [SVG, SVG1, SVG2, SVG3, SVG4, SVG5, SVG6];
+export default imageArr;
