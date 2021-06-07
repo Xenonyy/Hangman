e53ad31506125a137c393fbc390e2e946bc8b842
Xenon <gonda.armand@gmail.com>
2021-06-07 17:31:03 +0200

    Updated header logo and text.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ export default class Header extends React.Component {
     render() {
         return(
             <header id="header">
-                <img src={logo} alt="Supercharge logo" id="supercharge-logo"/>
-                <p style={{textTransform: "uppercase", filter: "invert(1)", fontWeight: "700"}}>Supercharge</p>
+                <img src={logo} alt="Hangman logo" id="hangman-logo"/>
+                <p style={{textTransform: "uppercase", filter: "invert(1)", fontWeight: "700"}}>Hangman</p>
             </header>
         )
     }
